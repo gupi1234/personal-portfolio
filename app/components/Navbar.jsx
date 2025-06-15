511e42b67f64cefe7843d1cb1f60c27083cc9df1
gupi debnath <iamgupidebntah@gmail.com>
2025-06-15 21:57:14 +0530

    hidden

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
 
   return (
     <>
-      <div className=" fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden">
+      <div className=" fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden hidden lg:block">
         <Image src={assets.header_bg_color} alt="err" className="w-full" />
       </div>
       <nav
