07175f5960c365cbe5d503f1261fe6a2f97ffa5a
gupi debnath <iamgupidebntah@gmail.com>
2025-06-18 19:16:42 +0530

    changeNav

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
   return (
     <>
       <div className=" fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden">
-        <Image src={assets.header_bg_color} alt="err" className="w-full" />
+        {/* <Image src={assets.header_bg_color} alt="err" className="w-full" /> */}
       </div>
       <nav
         className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex justify-between items-center z-50 ${
